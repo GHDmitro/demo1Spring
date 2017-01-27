11959361ac1a7586b89d14f07aaf180610746b02
GHDmitro <d.timosh1996@gmail.comd.timosh1996@gmail.comd.timosh1996@gmail.com>
2017-01-27 20:47:22 +0200

    Profile "batch" is displayed

--- src/main/java/pac/batch/GreetingBatchBean.java
+++ src/main/java/pac/batch/GreetingBatchBean.java
@@ -1,13 +1,13 @@
 package pac.batch;
 
-import pac.entities.Greeting;
-import pac.services.GreetingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import pac.entities.Greeting;
+import pac.services.GreetingService;
 
 import java.util.Collection;
 
